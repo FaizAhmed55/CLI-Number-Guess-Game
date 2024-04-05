bb9dff05c3dc48e342ceaada5704624381264647
FaizAhmed55 <159883350+FaizAhmed55@users.noreply.github.com>
2024-04-05 23:06:24 +0500

    some changes
    some changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+#! usr/bin/env node
 import inquirer from "inquirer";
 const systemguess = Math.floor(Math.random() * 10 + 1);
 const recall = await inquirer.prompt([
     {
-        name: "numberguessgame",
+        name: "userguessnumber",
         type: "number",
         message: "please guess a number between 1 to 10",
     },
 ]);
-if (recall.numberguessgame === systemguess) {
+if (recall.userguessnumber === systemguess) {
     console.log("Congaratulation! You Guess a Right Number");
 }
 else {
